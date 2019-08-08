81f734d92c5daed115a324b9bae270d3239ca2b4
Oksana2607 <k.sushalev26@gmail.com>
2019-08-08 22:38:29 +0300

    hw5

---
+++ BinaryTree/binaryTree.js
@@ -0,0 +1,286 @@
+﻿function ProtoBinaryTree() {
+    this.root = null;
+}
+
+ProtoBinaryTree.prototype.init = function () {
+};
+ProtoBinaryTree.prototype.getSize = function () {
+};
+ProtoBinaryTree.prototype.toString = function () {
+};
+ProtoBinaryTree.prototype.toArray = function () {
+};
+ProtoBinaryTree.prototype.push = function () {
+};
+ProtoBinaryTree.prototype.remove = function () {
+};
+ProtoBinaryTree.prototype.get = function () {
+};
+ProtoBinaryTree.prototype.set = function () {
+};
+ProtoBinaryTree.prototype.setBalance = function () {
+};
+
+function BinaryTree() {
+    ProtoBinaryTree.apply(this, arguments);
+}
+
+BinaryTree.prototype = Object.create(ProtoBinaryTree.prototype);
+BinaryTree.prototype.constructor = BinaryTree;
+
+BinaryTree.prototype.push = function (value) {
+    if (!this.root) {
+        if (value === undefined) {
+            return null;
+        } else {
+            this.root = new Node(value);
+
+            return this.getSize();
+        }
+    }
+
+    let currentNode = this.root;
+    let newNode = new Node(value);
+
+    while (currentNode) {
+        if (value < currentNode.value) {
+            if (!currentNode.left) {
+                currentNode.left = newNode;
+                break;
+            } else {
+                currentNode = currentNode.left;
+            }
+        } else {
+            if (!currentNode.right) {
+                currentNode.right = newNode;
+                break;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+    }
+
+    return this.getSize();
+};
+
+BinaryTree.prototype.init = function (array) {
+    if (!array || array.length < 1) {
+        return this.root;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        this.push(array[i]);
+    }
+
+    return this.root;
+};
+
+BinaryTree.prototype.toArray = function () {
+    let array = [];
+
+    if (!this.root) {
+        return [];
+    }
+
+    traverseTree(this.root);
+
+    function traverseTree(node) {
+
+        if (node.left) {
+            traverseTree(node.left);
+        }
+
+        array.push(node.value);
+
+        if (node.right) {
+            traverseTree(node.right);
+        }
+    }
+
+    return array;
+};
+
+BinaryTree.prototype.toString = function () {
+    if (!this.root) {
+        return '';
+    }
+
+    let result = '[';
+
+    traverseTree(this.root);
+
+    function traverseTree(node) {
+
+        if (node.left) {
+            traverseTree(node.left);
+        }
+
+        result += `${node.value}, `;
+
+        if (node.right) {
+            traverseTree(node.right);
+        }
+    }
+
+    result = result.slice(0, -2);
+
+    return result += ']';
+};
+
+BinaryTree.prototype.getSize = function () {
+    if (!this.root) {
+        return 0;
+    } else {
+        let size = 1;
+        let left = 0;
+        let right = 0;
+
+        traverseTree(this.root);
+
+        function traverseTree(node) {
+            if (node.left) {
+                traverseTree(node.left);
+                left++;
+            }
+
+            if (node.right) {
+                traverseTree(node.right);
+                right++;
+            }
+        }
+
+        return size + left + right;
+    }
+};
+
+BinaryTree.prototype.getHeight = function () {
+    let heightLeft = 0;
+    let heightRight = 0;
+
+    if (!this.root) {
+        return 0;
+    }
+
+    if (this.root.left) {
+        heightLeft = getNodeSize(this.root.left);
+    }
+
+    if (this.root.right) {
+        heightRight = getNodeSize(this.root.right);
+    }
+
+    if (heightLeft > heightRight) {
+        return 1 + heightLeft;
+    } else {
+        return 1 + heightRight;
+    }
+
+    function getNodeSize(node) {
+        let size = 1;
+        let left = 0;
+        let right = 0;
+
+        traverseTree(node);
+
+        function traverseTree(node) {
+            if (node.left) {
+                traverseTree(node.left);
+                left++;
+            }
+
+            if (node.right) {
+                traverseTree(node.right);
+                right++;
+            }
+        }
+
+        return size + left + right;
+    }
+};
+
+
+BinaryTree.prototype.remove = function (value) {
+    if (!this.root) {
+        return;
+    }
+
+    if (!value) {
+        return;
+    }
+
+    this.root = removeNode(this.root, value);
+
+    return this.root;
+};
+
+let removeNode = function (currentNode, value) {
+    if (!currentNode) {
+        currentNode = this.root;
+    }
+
+    if (currentNode.value === value) {
+        if (!currentNode.left && !currentNode.right) {
+
+            return null;
+        } else if (!currentNode.right) {
+
+            return currentNode.left;
+        } else if (!currentNode.left) {
+
+            return currentNode.right;
+        } else {
+            let leftMin = getMin(currentNode.right);
+            currentNode.value = leftMin.value;
+            currentNode.right = removeNode(currentNode.right, leftMin.value);
+
+            return currentNode;
+        }
+    } else if (value < currentNode.value) {
+        currentNode.left = removeNode(currentNode.left, value);
+
+        return currentNode;
+    } else if (value > currentNode.value) {
+        currentNode.right = removeNode(currentNode.right, value);
+
+        return currentNode;
+    }
+};
+
+let getMin = function (node) {
+    if (!node.left) {
+        return node;
+    }
+
+    if (node.left) {
+        return getMin(node.left);
+    }
+
+    return node.value;
+};
+
+BinaryTree.prototype.setBalance = function () {
+    let array = this.toArray();
+    let start = 0;
+    let end = array.length - 1;
+    this.root = createBalancedTree(array, start, end);
+
+    return this.root;
+};
+
+function createBalancedTree(array, start, end) {
+    if (end < start) {
+        return null;
+    }
+
+    const middle = Math.floor((start + end) / 2);
+    const node = new Node(array[middle]);
+    node.left = createBalancedTree(array, start, middle - 1);
+    node.right = createBalancedTree(array, middle + 1, end);
+
+    return node;
+}
+function Node(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
---
+++ BinaryTree/test/binaryTree.test.js
@@ -0,0 +1,512 @@
+﻿describe('init, getSize tests', () => {
+    let binaryTree;
+
+    beforeEach(() => {
+        binaryTree = new BinaryTree();
+    });
+
+    it(`should set nothing if undefined is initialized`, () => {
+        //Given
+        let array = undefined;
+        const expected = 0;
+
+        //When
+        binaryTree.init(array);
+
+        //Then
+        assert.deepEqual(binaryTree.getSize(), expected);
+    });
+
+    it(`should set nothing if null is initialized`, () => {
+        //Given
+        let array = null;
+        const expected = 0;
+
+        //When
+        binaryTree.init(array);
+
+        //Then
+        assert.deepEqual(binaryTree.getSize(), expected);
+    });
+
+    it(`should set nothing if [] is initialized`, () => {
+        //Given
+        let array = [];
+        const expected = 0;
+
+        //When
+        binaryTree.init(array);
+
+        //Then
+        assert.deepEqual(binaryTree.getSize(), expected);
+    });
+
+    it(`should set binaryTree if [2] is initialized`, () => {
+        //Given
+        let array = [2];
+        const expected = 1;
+
+        //When
+        binaryTree.init(array);
+
+        //Then
+        assert.deepEqual(binaryTree.getSize(), expected);
+    });
+
+    it(`should set binaryTree if [-5, 15] is initialized`, () => {
+        //Given
+        let array = [-5, 15];
+        const expected = 2;
+
+        //When
+        binaryTree.init(array);
+
+        //Then
+        assert.deepEqual(binaryTree.getSize(), expected);
+    });
+
+    it(`should set binaryTree if [35, 14, 18, 0, 3] is initialized`, () => {
+        //Given
+        let array = [35, 14, 18, 0, 3];
+        const expected = 5;
+
+        //When
+        binaryTree.init(array);
+
+        //Then
+        assert.deepEqual(binaryTree.getSize(), expected);
+    });
+});
+
+describe('toString tests', () => {
+    let binaryTree;
+
+    beforeEach(() => {
+        binaryTree = new BinaryTree();
+    });
+
+    it('should return "" if undefined is set', () => {
+        //Given
+        binaryTree.init(undefined);
+        const expected = '';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return "" if null is set', () => {
+        //Given
+        binaryTree.init(null);
+        const expected = '';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return "" if undefined is set', () => {
+        //Given
+        binaryTree.init(undefined);
+        const expected = '';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return "" if [] is set', () => {
+        //Given
+        binaryTree.init([]);
+        const expected = '';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return "[2]" if [2] is set', () => {
+        //Given
+        binaryTree.init([2]);
+        const expected = '[2]';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return "[5, 8]" if [5, 8] is set', () => {
+        //Given
+        binaryTree.init([5, 8]);
+        const expected = '[5, 8]';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return "[-7, -2, 5, 8, 15]" if [5, 8, -7, 15, -2] is set', () => {
+        //Given
+        binaryTree.init([5, 8, -7, 15, -2]);
+        const expected = '[-7, -2, 5, 8, 15]';
+
+        //When
+        const actual = binaryTree.toString();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+});
+
+describe('push tests', () => {
+    let binaryTree;
+
+    beforeEach(() => {
+        binaryTree = new BinaryTree();
+    });
+
+    it('should return null if undefined is pushed', () => {
+        //Given
+        const value = undefined;
+        const expected = null;
+
+        //When
+        const actual = binaryTree.push(value);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return null if null is pushed', () => {
+        //Given
+        const value = null;
+        const expected = 1;
+
+        //When
+        const actual = binaryTree.push(value);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return 1 if one element is pushed (2)', () => {
+        //Given
+        const value = 2;
+        const expected = 1;
+
+        //When
+        const actual = binaryTree.push(value);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return 3 if one element is pushed (15) to [5, 12]', () => {
+        //Given
+        binaryTree.init([5, 12]);
+
+        const value = 15;
+        const expected = 3;
+
+        //When
+        const actual = binaryTree.push(value);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return 6 if one element is pushed (100) to [5, 12, -34, 45, -7]', () => {
+        //Given
+        binaryTree.init([5, 12, -34, 45, -7]);
+
+        const value = 100;
+        const expected = 6;
+
+        //When
+        const actual = binaryTree.push(value);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+});
+
+
+describe('toArray tests', () => {
+    let binaryTree;
+
+    beforeEach(() => {
+        binaryTree = new BinaryTree();
+    });
+
+    it('should return [] if undefined is set', () => {
+        //Given
+        binaryTree.init(undefined);
+        const expected = [];
+
+        //When
+        const actual = binaryTree.toArray();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return [] if null is set', () => {
+        //Given
+        binaryTree.init(null);
+        const expected = [];
+
+        //When
+        const actual = binaryTree.toArray();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return [] if [] is set', () => {
+        //Given
+        binaryTree.init([]);
+        const expected = [];
+
+        //When
+        const actual = binaryTree.toArray();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return [2] if [2] is set', () => {
+        //Given
+        binaryTree.init([2]);
+        const expected = [2];
+
+        //When
+        const actual = binaryTree.toArray();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return [5, 8] if [5, 8] is set', () => {
+        //Given
+        binaryTree.init([5, 8]);
+        const expected = [5, 8];
+
+        //When
+        const actual = binaryTree.toArray();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should return [5, 8, -7, 15, -2] if [-7, -2, 5, 8, 15] is set', () => {
+        //Given
+        binaryTree.init([5, 8, -7, 15, -2]);
+        const expected = [-7, -2, 5, 8, 15];
+
+        //When
+        const actual = binaryTree.toArray();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+});
+
+describe('remove tests', () => {
+    let binaryTree;
+
+    beforeEach(() => {
+        binaryTree = new BinaryTree();
+    });
+
+    it('should return "" if undefined is initialized', () => {
+        //Given
+        binaryTree.init();
+        const expected = '';
+        const expectedSize = 0;
+
+        //When
+        binaryTree.remove();
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+
+    it('should return "" if null is initialized', () => {
+        //Given
+        binaryTree.init(null);
+        const expected = '';
+        const expectedSize = 0;
+
+        //When
+        binaryTree.remove();
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+
+    it('should return "" if null is initialized', () => {
+        //Given
+        binaryTree.init(null);
+        const expected = '';
+        const expectedSize = 0;
+
+        //When
+        binaryTree.remove(5);
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+
+    it('should return "" if [] is initialized', () => {
+        //Given
+        binaryTree.init([]);
+        const expected = '';
+        const expectedSize = 0;
+
+        //When
+        binaryTree.remove();
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+
+    it('should return "" if [4] is initialized', () => {
+        //Given
+        binaryTree.init([4]);
+        const expected = '';
+        const expectedSize = 0;
+
+        //When
+        binaryTree.remove(4);
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+
+    it('should return "[8]" if [5, 8] is initialized', () => {
+        //Given
+        binaryTree.init([5, 8]);
+        const expected = '[8]';
+        const expectedSize = 1;
+
+        //When
+        binaryTree.remove(5);
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+
+    it('should return "[-2, 5, 8, 15]" if [5, 8, -7, 15, -2] is initialized', () => {
+        //Given
+        binaryTree.init([5, 8, -7, 15, -2]);
+        const expected = '[-2, 5, 8, 15]';
+        const expectedSize = 4;
+
+        //When
+        binaryTree.remove(-7);
+
+        //Then
+        assert.deepEqual(binaryTree.toString(), expected);
+        assert.deepEqual(binaryTree.getSize(), expectedSize);
+    });
+});
+
+describe('setBalance tests', () => {
+    let binaryTree = new BinaryTree();
+
+    it(`should get different heights`, () => {
+        //Given
+        binaryTree.init([-10, 1, 2, 3, 8, -2]);
+        const expected = binaryTree.getHeight();
+
+        //When
+        binaryTree.setBalance();
+        const actual = binaryTree.getHeight();
+
+        //Then
+        assert.operator(actual, '<', expected);
+    });
+
+    it('should get different heights', () => {
+        //Given
+        binaryTree.init([1, 15, 7, 18, 2, -3]);
+        const expected = binaryTree.getHeight();
+
+        //When
+        binaryTree.setBalance();
+        const actual = binaryTree.getHeight();
+
+        //Then
+        assert.operator(actual, '<', expected);
+    });
+
+    it('should get different heights', () => {
+        //Given
+        binaryTree.init([0, 4, -7, -5, 15, 18, 35]);
+        const expected = binaryTree.getHeight();
+
+        //When
+        binaryTree.setBalance();
+        const actual = binaryTree.getHeight();
+
+        //Then
+        assert.operator(actual, '<', expected);
+    });
+
+    it('should get different heights', () => {
+        //Given
+        binaryTree.init([2]);
+        const expected = binaryTree.getHeight();
+
+        //When
+        binaryTree.setBalance();
+        const actual = binaryTree.getHeight();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should get different heights', () => {
+        //Given
+        binaryTree.init([2, -2]);
+        const expected = binaryTree.getHeight();
+
+        //When
+        binaryTree.setBalance();
+        const actual = binaryTree.getHeight();
+
+        //Then
+        assert.deepEqual(actual, expected);
+    });
+
+    it('should get different heights', () => {
+        //Given
+        binaryTree.init([2, -2, 15]);
+        const expected = binaryTree.getHeight();
+
+        //When
+        binaryTree.setBalance();
+        const actual = binaryTree.getHeight();
+
+        //Then
+        assert.operator(actual, '<', expected);
+    });
+});
